200e561f849253ffdc2fe5d999cae52f39cb057d
이윤수 <lws8905@gmail.com>
2021-12-19 19:42:21 +0900

    20211219

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print('hellow')
+##2021.12.19(자료형1)
+print(1+1)
+a = 5
+print(type (a))
+print ("ㅎㅇ")
+
+b= "Life is too short, you need Python"
+print(b[0])
+print(b[2])
+
+c="20010331Rainy"
+print(c[:8])
